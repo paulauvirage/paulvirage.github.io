c9322f1ab9e47a5ac1f4529792f174bdb6b4d316
paulauvirage <spiloptila@gmail.com>
2024-08-19 12:17:48 +0000

    added text and immage to landing page

--- setup_notes.r
+++ setup_notes.r
@@ -3,4 +3,6 @@
 #' in terminal ctrl+c to close preview, as well as pressing the red terminal button. Need to close terinal, or open a second, new, terminal, to continue with the instructions.
 #'Not PATs - see Happy git with R - and potential complication of PAT being recognizable on Linux (HGR has a solution)
 #'touch .nojekyll does create the file, bu it is not visible in the Files listing (R Studio bottom right window, as it is a . file?), but does show up in the git tab. You can confirm it is in your project directory by typing ls -a (include dot files) in the terminal.
-#'quarto render created the website as a docs folder, but did not make it visible  
+#'quarto render created the website as a docs folder, but did not make it visible. Clicking on build - render website (or using the shortcut ctrl+shift+b) does make it visible in the viewer.
+#'i decided to skip 5 (Configure GitHub pages and launch website) until i'd got some more design and content.
+#'
