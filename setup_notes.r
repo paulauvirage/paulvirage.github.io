b64e7eb422f55bdbc07f310bfddc8d99986beed0
paulauvirage <spiloptila@gmail.com>
2024-08-19 11:26:01 +0000

    Commit unedited website

---
+++ setup_notes.r
@@ -0,0 +1,6 @@
+#' https://ucsb-meds.github.io/creating-quarto-websites/#resources
+#' does not check "creat github repo"
+#' in terminal ctrl+c to close preview, as well as pressing the red terminal button. Need to close terinal, or open a second, new, terminal, to continue with the instructions.
+#'Not PATs - see Happy git with R - and potential complication of PAT being recognizable on Linux (HGR has a solution)
+#'touch .nojekyll does create the file, bu it is not visible in the Files listing (R Studio bottom right window, as it is a . file?), but does show up in the git tab. You can confirm it is in your project directory by typing ls -a (include dot files) in the terminal.
+#'quarto render created the website as a docs folder, but did not make it visible  
