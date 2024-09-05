c77823ccd91ebab77492185c85d8b0e970f928c5
Paul Robinson <spiloptila@gmail.com>
2024-09-05 13:31:24 +0100

    notes compare rstanarm, brms, lm

---
+++ brms-notes.r
@@ -0,0 +1,26 @@
+library(brms)
+library(abd)
+library(broom.mixed)
+
+lion_noses <- as.data.frame(LionNoses)
+
+lion_noses <- lion_noses %>% 
+    mutate(percentage.black = 100*proportion.black)
+
+m3 <- brm(
+    age ~ percentage.black,
+    data = lion_noses,
+    family = gaussian
+)
+
+stancode(age ~ percentage.black, data = lion_noses)
+
+plot(m3)
+
+print(m3, prob = .9)
+
+broom.mixed::tidy(m3, conf.level = 0.9)
+
+results <- as.data.frame(round(posterior_summary(m3, probs = c(0.05, 0.95))[1:3,],3))
+
+
---
+++ glm-notes.r
@@ -0,0 +1,21 @@
+m4 <- glm(age ~ percentage.black, data = lion_noses, family = gaussian)
+
+broom::tidy(m4, prob = 0.9, effects = c("fixed", "aux"))
+
+broom::glance(m4)
+
+round(confint(m4, level = 0.9), 3)
+
+### from ISLR 3.6.2
+
+m5 <- lm(age ~ percentage.black, data = lion_noses)
+
+m5
+
+summary(m5)
+
+names(m5)
+
+round(confint(m5, level = 0.9), 3)
+
+round(predict (m5, data.frame(percentage.black = (seq(10,80,10))), interval = "confidence", level = 0.9), 3)
